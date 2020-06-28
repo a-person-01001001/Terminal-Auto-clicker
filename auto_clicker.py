93047334e2f0e394a8c5e32666d8ea420f557c72
a-person-01001001 <51980001+a-person-01001001@users.noreply.github.com>
2020-06-28 12:04:55 -0500

    Adding auto_clicker.py
    This is the actual auto clicker.

---
+++ auto_clicker.py
@@ -0,0 +1,10 @@
+import pyautogui
+from pynput import keyboard
+
+width, height = pyautogui.size()
+pauselength = float(input("PAUSE: "))
+while True:
+    pyautogui.FAILSAFE = True
+    if 
+    pyautogui.click(pyautogui.position())
+    pyautogui.PAUSE = pauselength
