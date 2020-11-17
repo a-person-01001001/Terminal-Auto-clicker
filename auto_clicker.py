ce78a8df036157281c4ea1cfc919caba90715b01
a-person-01001001 <51980001+a-person-01001001@users.noreply.github.com>
2020-11-16 19:50:28 -0600

    Update auto_clicker.py

--- auto_clicker.py
+++ auto_clicker.py
@@ -1,10 +1,8 @@
 import pyautogui
-from pynput import keyboard
 
 width, height = pyautogui.size()
 pauselength = float(input("PAUSE: "))
 while True:
     pyautogui.FAILSAFE = True
-    if 
     pyautogui.click(pyautogui.position())
     pyautogui.PAUSE = pauselength
